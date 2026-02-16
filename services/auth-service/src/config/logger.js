93781f2abaad2ba96a36e5930ccaa7b9318b242f
Hydra-Ramesh <dasramesh8343003905@gmail.com>
2026-02-16 20:01:09 +0530

    Update Logger File

--- services/auth-service/src/config/logger.js
+++ services/auth-service/src/config/logger.js
@@ -2,14 +2,9 @@ import winston from 'winston';
 import env from './env.js';
 
 const logger = winston.createLogger({
-    level: env === 'production' ? 'info' : 'debug',
-    format: winston.format.combine(
-        winston.format.timestamp(),
-        winston.format.json()
-    ),
-    transports: [
-        new winston.transports.Console()
-    ]
+  level: env === 'production' ? 'info' : 'debug',
+  format: winston.format.combine(winston.format.timestamp(), winston.format.json()),
+  transports: [new winston.transports.Console()],
 });
 
 export default logger;
