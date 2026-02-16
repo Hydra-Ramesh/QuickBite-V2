3c09d280fda68b2c37fe8b9e6543c16eade48917
Hydra-Ramesh <dasramesh8343003905@gmail.com>
2026-02-16 23:53:39 +0530

    Error Handler is Ready to Check

---
+++ services/auth-service/src/middlewares/error.middleware.js
@@ -0,0 +1,9 @@
+import logger from '../config/logger.js';
+
+export default (err, req, res, next) => {
+    logger.error(err.message);
+    res.status(err.statusCode || 500).json({
+        success: false,
+        message: err.message || 'Internal Server Error',
+    });
+};
