78bab69f8f04308be28056928c06ac819c418c37
Hydra-Ramesh <dasramesh8343003905@gmail.com>
2026-02-16 19:54:35 +0530

    Add Advance Logger File

---
+++ services/auth-service/src/config/logger.js
@@ -0,0 +1,15 @@
+import winston from 'winston';
+import env from './env.js';
+
+const logger = winston.createLogger({
+    level: env === 'production' ? 'info' : 'debug',
+    format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.json()
+    ),
+    transports: [
+        new winston.transports.Console()
+    ]
+});
+
+export default logger;
